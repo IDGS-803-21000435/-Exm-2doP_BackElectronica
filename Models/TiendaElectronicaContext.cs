4e740cd48557d3ed2361081bc9f6e9ee8e4cba8a
VicMac003 <vhmedinac03@gmail.com>
2024-07-05 23:20:48 -0600

    first commit

---
+++ Models/TiendaElectronicaContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackElectronicos.Models;
+
+public partial class TiendaElectronicaContext : DbContext
+{
+    public TiendaElectronicaContext()
+    {
+    }
+
+    public TiendaElectronicaContext(DbContextOptions<TiendaElectronicaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producto__3214EC0706E1DFE8");
+            entity.ToTable("Productos");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Precio).HasColumnType("decimal(10, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using BackElectronicos.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var connecionString = builder.Configuration.GetConnectionString("cadenaSQL");
+
+//Agregamos la configuración para SQL
+builder.Services.AddDbContext<TiendaElectronicaContext>(options =>
+    options.UseSqlServer(connecionString)
+);
+
+
+//Definimos la nueva politica de CORS
+builder.Services.AddCors(options =>
+{
+
+    options.AddPolicy("NuevaPolitica", app =>
+    {
+        app.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Activamos la nueva politica
+app.UseCors("NuevaPolitica");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+app.Run();
+
+
